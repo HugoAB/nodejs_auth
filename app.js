294510733a0a3dd55894d7039b04205685de81f5
HugoAB <hugoabobadillav@outlook.com>
2021-04-22 23:22:40 -0400

    Added routes

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const db = mysql.createConnection({
   database: process.env.DATABASE
 });
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 const publicDirectory = path.join(__dirname, "./public");
 app.use(express.static(publicDirectory));
 
@@ -28,9 +31,9 @@ db.connect((err) => {
   }
 })
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// Routes
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
 
 app.listen(3000, () => {
   console.log("App running on port 3000");
---
+++ controllers/auth.js
@@ -0,0 +1,8 @@
+
+const register = (req, res) => {
+    res.send("FOrm submitted");
+}
+
+module.exports = {
+    register
+}
